0f987e9140f78814c2dc33524c6d1df3f84c9872
Naoya Akaike <akaike@tokyo-ict.com>
2022-03-07 19:17:04 +0900

    delete demo data

--- springboot/demo/src/main/java/com/example/demo/MainController.java
+++ springboot/demo/src/main/java/com/example/demo/MainController.java
@@ -44,28 +44,6 @@ public class MainController {
         data.setUpdatedDate(Date.valueOf(LocalDate.now()));
         repository.saveAndFlush(data);
         return new ModelAndView("redirect:/");
-        
-        /*デモデータ
-        TodoData d1 = new TodoData();
-        d1.setTodo("running");
-        d1.setKijitsu(Date.valueOf("2020-07-20"));
-        d1.setSts(1);
-        d1.setDeleteFlg(0);
-        d1.setCreatedDate(Date.valueOf("2020-07-10"));
-        d1.setUpdatedDate(Date.valueOf("2020-07-15"));
-
-        TodoData d2 = new TodoData();
-        d2.setTodo("read some books");
-        d2.setKijitsu(Date.valueOf("2020-07-31"));
-        d2.setSts(1);
-        d2.setDeleteFlg(0);
-        d2.setCreatedDate(Date.valueOf("2020-07-20"));
-        d2.setUpdatedDate(Date.valueOf("2020-07-20"));
-
-
-        repository.saveAndFlush(d1);
-        repository.saveAndFlush(d2);
-        return new ModelAndView("redirect:/");// 格納した配列*/
     }
 
     @RequestMapping("/delete")
